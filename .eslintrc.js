514e522df00cf3f4b24420d3afee734bf42ddf21
Vadim Kasatkin <penguin2804@gmail.com>
2021-06-26 20:45:52 +0400

    fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-    "prettier"
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    "import/no-unresolved": "off",
-    "import/extensions": ["warn", "never"]
+    'import/no-unresolved': 'off',
+    'import/extensions': ['warn', 'never'],
   },
 };
